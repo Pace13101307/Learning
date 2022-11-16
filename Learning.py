d6b37ff276c3c66be4d15f1e6bd871f1f6dcd63a
SamuelPace <60515550+Pace13101307@users.noreply.github.com>
2022-11-16 19:26:57 +0000

    Udemy4
    part 4 static and class methods

--- Learning.py
+++ Learning.py
@@ -46,10 +46,33 @@ class PlayerCharacter:
   
   def shout(self):
     print(f"my name is {self.name}")
+  
+  # classmethods can be used even without instantiating
+  # the class
+  # but requires "cls" instead of self, as an argument
+  # we can use the cls to instantiate the class
+  # classmethod when you do care about the class attrs
+  @classmethod
+  def adding_things(cls, num1, num2):
+    return cls("Teddy", num1 + num2)
+    # here we instantiate the class
+  
+  @staticmethod
+  def adding_things_static(num1, num2):
+    return num1 + num2
+    # staticmethod works the same as classmethod
+    # however you do not have access to the class
+    # staticmethod when you don't care about the class
+    # attributes
 
 player_1 = PlayerCharacter("kyle", 24)
 player_2 = PlayerCharacter("bob", 53)
 player_2.attack = 50
+
+player_3 = PlayerCharacter.adding_things(2, 3)
+print(player_3.age)
+# using classmethod we now instantiated a class from
+# within the class using the parameters in the function
 
 print(player_1.name)
 print(player_2.age)
