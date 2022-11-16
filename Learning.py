0e9f9e6d95a3d9aa6faf1cfaa3b8512e26657397
SamuelPace <60515550+Pace13101307@users.noreply.github.com>
2022-11-16 18:33:28 +0000

    Udemy2
    part 2

--- Learning.py
+++ Learning.py
@@ -15,11 +15,28 @@ and make separate instances of a class
 
 it's the same like making multiple lists, 
 giving me access to methods of class list
+
+1 class will allow for many instances 
+which will live in different places in memory
 """
 
 class PlayerCharacter:
-  def __init__(self, name):
+  def __init__(self, name, age):
     self.name = name
+    self.age = age
   
   def run(self):
     print("Run")
+    return "done"
+
+player_1 = PlayerCharacter("kyle", 24)
+player_2 = PlayerCharacter("bob", 53)
+player_2.attack = 50
+
+print(player_1.name)
+print(player_2.age)
+player_1.run()
+print(player_1.attack)
+print(player_2.attack)
+
+# 
